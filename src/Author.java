28cec07a72f2bb33e29e4987b9a71b00f4f39104
Pavel <hareharrionh@gmail.com>
2023-04-30 21:17:07 +0300

    Almost done

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     String fullName;
 
@@ -6,6 +8,24 @@ public class Author {
     }
     public String getFullName() {
         return fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "fullName=" + fullName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(fullName, author.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName);
     }
 }
 
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 public class Book {
     String bookName;
     Author author;
@@ -7,19 +8,32 @@ public class Book {
         this.author = author;
         this.releaseYear = releaseYear;
     }
+    @Override
+    public String toString() {
+        return "bookName=" + this.bookName + ", " + this.author + ", releaseYear=" + this.releaseYear;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return releaseYear == book.releaseYear && Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, author, releaseYear);
+    }
     public String getAuthor() {
         return author.getFullName();
     }
-
     public int getReleaseYear() {
         return releaseYear;
     }
-
     public String getBookName() {
         return bookName;
     }
-
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
     }
